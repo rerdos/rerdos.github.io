d42ce8524854e9ccfbe9caa443a3aea45a454149
Richard Erdos <rerdos@Richards-epic-Apple-station.local>
2015-06-06 23:11:18 +0200

    $http data load

--- finalexam/scripts/main.js
+++ finalexam/scripts/main.js
@@ -1,1 +1,18 @@
-angular.module('finalExam', []);
+'use strict';
+mainCtrl.$inject = ['$scope', '$http'];
+function mainCtrl($scope, $http) {
+    $scope.testTypes = [];
+
+    $http.get('data/data.json').
+        success(function(data) {
+            $scope.testTypes = data;
+        }).
+        error(function() {
+            console.error("finalExam app: Error during loading the data file!");
+            alert("Error during loading the data file!");
+        });
+}
+
+angular.module('finalExam', [])
+    .controller('mainCtrl', mainCtrl);
+
