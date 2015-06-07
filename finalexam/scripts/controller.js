9dba5739cd3d1a70d25ea177421e2020ebdb394c
rerdos <ricsi96@gmail.com>
2015-06-07 15:51:01 +0200

    Site phrases and edit toggle

--- finalexam/scripts/controller.js
+++ finalexam/scripts/controller.js
@@ -3,6 +3,17 @@ function mainCtrl($scope, $http, menuData) {
     $scope.testTypes = [];
     $scope.menuData = menuData;
     $scope.activeMenuIndex = 0;
+    $scope.selectMenu = selectMenu;
+    $scope.isShowEdit = false;
+    $scope.toggleEdit = toggleEdit;
+
+    function selectMenu(id) {
+        $scope.activeMenuIndex = id;
+    }
+
+    function toggleEdit() {
+        $scope.isShowEdit = !$scope.isShowEdit;
+    }
 
     $http.get('data/data.json').
         success(function(data) {
