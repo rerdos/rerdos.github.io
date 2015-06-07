828c25abff520b332806ee15f2eb27f8bf798b29
rerdos <ricsi96@gmail.com>
2015-06-07 17:40:52 +0200

    Made adding form functionable

--- finalexam/scripts/controller.js
+++ finalexam/scripts/controller.js
@@ -1,11 +1,16 @@
-mainCtrl.$inject = ['$scope', '$http', 'menuData'];
-function mainCtrl($scope, $http, menuData) {
+mainCtrl.$inject = ['$scope', '$http', 'menuData', 'errorMessages'];
+function mainCtrl($scope, $http, menuData, errorMessages) {
     $scope.testTypes = [];
     $scope.menuData = menuData;
     $scope.activeMenuIndex = 0;
     $scope.selectMenu = selectMenu;
     $scope.isShowEdit = false;
     $scope.toggleEdit = toggleEdit;
+    $scope.saveExam = saveExam;
+    $scope.exam = {examName: "",type: 0, writtenTotal: "", writtenEarned: "", total: ""};
+    $scope.error = false;
+
+    $scope.exams = [];
 
     function selectMenu(id) {
         $scope.activeMenuIndex = id;
@@ -13,6 +18,35 @@ function mainCtrl($scope, $http, menuData) {
 
     function toggleEdit() {
         $scope.isShowEdit = !$scope.isShowEdit;
+    }
+
+    function saveExam() {
+        if (formValidator()===0) {
+            $scope.error = false;
+            $scope.exams.push(angular.copy($scope.exam));
+            $scope.exams[$scope.exams.length-1].id = $scope.exams.length-1;
+            toggleEdit();
+            $scope.exam = {examName: "",type: 0, writtenTotal: "", writtenEarned: "", total: ""};
+        } else {
+            $scope.error = true;
+            $scope.errorMessage = errorMessages[formValidator()-1].error;
+        }
+    }
+
+    function formValidator() {
+        if ($scope.exam.examName.length>50) {
+            return(1);
+        } else if ($scope.exam.examName==="" || $scope.exam.writtenTotal==="" || $scope.exam.writtenEarned==="" || $scope.exam.total ==="") {
+            return (2);
+        } else if (isNaN($scope.exam.writtenTotal) || isNaN($scope.exam.writtenEarned) || isNaN($scope.exam.total)) {
+            return(3);
+        } else if (parseInt($scope.exam.writtenTotal)<parseInt($scope.exam.writtenEarned)) {
+            return(4);
+        } else if (parseInt($scope.exam.writtenTotal)>parseInt($scope.exam.total)) {
+            return(5);
+        } else {
+            return(0);
+        }
     }
 
     $http.get('data/data.json').
--- finalexam/scripts/factory.js
+++ finalexam/scripts/factory.js
@@ -10,4 +10,28 @@ angular.module('finalExam')
                 "name": "Info"
             }
         ];
+    })
+    .factory('errorMessages', function() {
+        return [
+            {
+                "id": 1,
+                "error": "The name of the exam cannot be more than 50 characters."
+            },
+            {
+                "id": 2,
+                "error": "You must fill every input field."
+            },
+            {
+                "id": 3,
+                "error": "You have written text somewhere where you shouldn't."
+            },
+            {
+                "id": 4,
+                "error": "Earned points in the written section cannot be greater than the total points of written exam."
+            },
+            {
+                "id": 5,
+                "error": "Points of the written exam cannot be greater than the total points."
+            }
+        ];
     });
