768b3ffabb812b22a9bdbfc3e3cf601ea6c6f80d
rerdos <ricsi96@gmail.com>
2015-06-09 21:24:32 +0200

    Add way to remove a todo

--- todo/main.js
+++ todo/main.js
@@ -1,7 +1,17 @@
 (function(){
     mainCtrl.$inject = ['$scope', '$http'];
     function mainCtrl($scope, $http) {
+        $scope.isEmpty = isEmpty;
+        $scope.removeTodo = removeTodo;
         $scope.todos = [];
+
+        function isEmpty() {
+            return !($scope.todos.length > 0);
+        }
+
+        function removeTodo(index) {
+            $scope.todos.splice(index, 1);
+        }
 
         $http.get('data.json')
             .success(function(data) {
