e3fa639cc99fa75e03b1fc293700141afc1cd9f8
rerdos <ricsi96@gmail.com>
2015-06-11 11:40:20 +0200

    Make edit mode toggle able

--- todo/main.js
+++ todo/main.js
@@ -1,13 +1,14 @@
 (function(){
     mainCtrl.$inject = ['$scope', '$http'];
     function mainCtrl($scope, $http) {
-        $scope.isEdit = {"value": false, "id": 0};
+        $scope.isEdit = {"value": false, "id": null};
         $scope.isEmpty = isEmpty;
         $scope.isValid = isValid;
         $scope.newTodo = {};
         $scope.removeTodo = removeTodo;
         $scope.todos = [];
         $scope.toggleDone = toggleDone;
+        $scope.toggleEdit = toggleEdit;
 
         function isEmpty() {
             return $scope.todos.length === 0;
@@ -20,13 +21,24 @@
                 return false;
             }
         }
-
+        
         function removeTodo(index) {
             $scope.todos.splice(index, 1);
         }
 
         function toggleDone(index) {
             $scope.todos[index].done = !$scope.todos[index].done;
+        }
+
+        function toggleEdit(index) {
+            if (!$scope.isEdit.value || $scope.isEdit.id !== $scope.todos[index].id) {
+                $scope.isEdit.value = true;
+                $scope.isEdit.id = $scope.todos[index].id;
+                $scope.newTodo = angular.copy($scope.todos[index]);
+            } else {
+                $scope.isEdit.value = false;
+                $scope.newTodo = {};
+            }
         }
 
         $http.get('data.json')
